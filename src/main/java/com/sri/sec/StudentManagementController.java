3990b6493b9ea90dbf1d2b77eb4748e67e83e3e8
sri <only4programming03@gmail.com>
2021-06-15 22:42:31 -0400

    @PreAuthorize and @EnableGlobalMethodSecurity(prePostEnabled=true) on controller class

--- src/main/java/com/sri/sec/StudentManagementController.java
+++ src/main/java/com/sri/sec/StudentManagementController.java
@@ -3,6 +3,8 @@ package com.sri.sec;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,29 +16,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/management/api/v1/students")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class StudentManagementController {
 
 	private static final List<Student> STUDENTS = Arrays.asList(new Student(1, "James Bond"),
 			new Student(2, "Maria Jones"), new Student(3, "Anna Smith"));
 
 	@GetMapping
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMINTRAINEE')")
 	public List<Student> getAllStudents() {
 		return STUDENTS;
 	}
 
 	@PostMapping
+	@PreAuthorize("hasAuthority('student:write')")
 	public void addStudent(@RequestBody Student student) {
 
 		System.out.println("Adding student " + student);
 	}
 
 	@PutMapping("/{studentId}")
+	@PreAuthorize("hasAuthority('student:write')")
 	public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
 
 		System.out.println("Updating student");
 	}
 
 	@DeleteMapping("/{studentId}")
+	@PreAuthorize("hasAuthority('student:write')")
 	public void deleteStudentById(@PathVariable("studentId") Integer studentId) {
 		System.out.println("deleting student with id ");
 	}
